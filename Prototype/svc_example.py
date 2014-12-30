bbd487a65f7697d060237b5ffab1bd23cd546766
Mohamed Talaat Harb <mohamed.talaatharb@eng.asu.edu.eg>
2014-12-30 14:13:26 +0200

    Fixed error in svc_example

--- Prototype/svc_example.py
+++ Prototype/svc_example.py
@@ -4,7 +4,7 @@ import numpy as np
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
 from sklearn.svm import SVC
->clf = SVC()
+clf = SVC()
 clf.fit(X, y) 
 
 print(clf.predict([[-0.8, -1]]))
